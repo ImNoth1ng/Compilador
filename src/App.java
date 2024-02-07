db2c0d8c75ed930b9d482af9972469f9ea492eb2
Noth1ng <luisbalcazar305@gmail.com>
2024-02-06 19:37:55 -0600

    Add file read/write functionality

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import ReadWriter.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String ruta = "/home/noth1ng/Documents/Compiladores";
+
+        ReadWriter testo = new ReadWriter(ruta);
+
+        testo.escribe("Hola tonotos, como estan\nEspero que bien", "archivoimportante.fes");
+        testo.lee("archivoimportante.fes");
+        testo.cuentaPalabras("archivoimportante.fes");
     }
 }
+
